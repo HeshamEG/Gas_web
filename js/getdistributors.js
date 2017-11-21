54dff8bd08137a7e050f9fb903a220504487a48c
Berson1990 <Berson1990@users.noreply.github.com>
2017-11-21 16:31:40 +0200

    commit - request

--- js/getdistributors.js
+++ js/getdistributors.js
@@ -1,4 +1,5 @@
 (function () {
+
     const config = {
         apiKey: "AIzaSyAzmtOBtWYDL4srdRqEfZFiwsq1rs2JHOA",
         authDomain: "gasksa-de4ab.firebaseapp.com",
@@ -7,20 +8,42 @@
         storageBucket: "gasksa-de4ab.appspot.com",
         messagingSenderId: "193816669511"
     };
+
+
     firebase.initializeApp(config);
+
+    var request = $('#RequestTable').DataTable();
 
     var dbRef = firebase.database();
     var contactsRef = dbRef.ref('distributors');
-    contactsRef.on('value',function (snapshot) {
-      console.log('data',snapshot.val())
+    contactsRef.on('value', function (snapshot) {
+        var distributor = [];
+        for (var i  in snapshot.val()) {
+            distributor.push(snapshot.val()[i]);
+
+        }
+
+        for (var t = 0; t < distributor.length; t++) {
+            tbody = '<tr>' +
+                '<td>' + distributor[t].name + '</td>' +
+                '<td>' + distributor[t].email + '</td>' +
+                '<td>' + distributor[t].phoneNo + '</td>' +
+                '<td>' +'<button class="btn btn-success" onclick="disstrupit()" >&#x2705;</button>'+'&nbsp;&nbsp;&nbsp;'+'<button class="btn btn-danger" onclick="disstrupit()">X</button>'+'</td>' +
+                '</tr>';
+            request.row.add($(tbody)).draw();
+        }
+
+        console.log(distributor);
+
+
     });
 
+    function disstrupit() {
+        alert();
+    }
 
     var userId = firebase.auth()
-    console.log('data',userId);
+    // console.log(userId);
 
-    return firebase.database().ref('/users/' + userId).on('value',function(snapshot) {
-        var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-        // ...
-    });
+
 }());
