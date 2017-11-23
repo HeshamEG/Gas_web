af247fab58505eedc44c14b2fc3ce55323d32fb3
HeshamEG <heshamsalama1212901022@gmail.com>
2017-11-23 08:56:36 +0200

    history

--- js/distHistory.js
+++ js/distHistory.js
@@ -26,42 +26,19 @@ console.log(i)
 
         }
         for (var t = 0; t < history.length; t++) {
-            console.log(history[t])
+            console.log(history[t]);
             var details= dbRef.ref('history/'+history[t]);
             details.once('value').then(function (snapshot){
                var refCustomerName= dbRef.ref('customers/'+snapshot.val().customerID+'/name');
-                refCustomerName.once('value').then(function (name){
-                    console.log(name.val())
 
-                })
-                console.log(snapshot.val())
+               refCustomerName.once('value').then(function (name){
+                    console.log(name.val());
+                });
+                console.log(snapshot.val());
 
             })
 
         }
-        // for (var t = 0; t < distributor.length; t++) {
-        //
-        //
-        //     tbody = '<tr>' +
-        //         '<td>' + distributor[t].name + '</td>' +
-        //         '<td>' + '<button class="btn btn-primary" onclick="hover(' + t + ')">صور الموزعين</button>' + '</td>' +
-        //         '<td>' + distributor[t].email + '</td>' +
-        //         '<td>' + distributor[t].phoneNo + '</td>' +
-        //         '<td>' + '<button class="btn btn-success" onclick="accceptordecliend(' + t + ',1)">&#x2705;</button>' + '&nbsp;&nbsp;&nbsp;' + '<button class="btn btn-danger" onclick="accceptordecliend(' + t + ',2)">X</button>' + '</td>' +
-        //         '</tr>';
-        //     request.row.add($(tbody)).draw();
-        //
-        //     // accceptordecliend = function (params, state) {
-        //     //
-        //     //     console.log(params);
-        //     //     contactsRef.child(keys[params] + '/state').set(state);
-        //     //     $.notify("تم بنجاح");
-        //     //     location.reload();
-        //     // };
-        //
-        // }
-
-
 
 
     });
