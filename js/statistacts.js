d3219522249c56dafbb34fc227d09092843ba0df
HeshamEG <heshamsalama1212901022@gmail.com>
2017-11-29 16:08:30 +0200

    s

---
+++ js/statistacts.js
@@ -0,0 +1,18 @@
+(function () {
+
+    const config = {
+        apiKey: "AIzaSyAzmtOBtWYDL4srdRqEfZFiwsq1rs2JHOA",
+        authDomain: "gasksa-de4ab.firebaseapp.com",
+        databaseURL: "https://gasksa-de4ab.firebaseio.com",
+        projectId: "gasksa-de4ab",
+        storageBucket: "gasksa-de4ab.appspot.com",
+        messagingSenderId: "193816669511"
+    };
+
+
+    firebase.initializeApp(config);
+    var ref = firebase.database().ref("history");
+    ref.orderByKey().startAt("date").on("child_added", function(snapshot) {
+        console.log(snapshot.key);
+    });
+}())
