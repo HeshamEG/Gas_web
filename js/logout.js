b10ee573f7f58e0dc5260574cffde11816316ec3
HeshamEG <heshamsalama1212901022@gmail.com>
2017-11-30 11:47:46 +0200

    logut

---
+++ js/logout.js
@@ -0,0 +1,20 @@
+(function () {
+
+    const config = {
+        apiKey: "AIzaSyAzmtOBtWYDL4srdRqEfZFiwsq1rs2JHOA",
+        authDomain: "gasksa-de4ab.firebaseapp.com",
+        databaseURL: "https://gasksa-de4ab.firebaseio.com",
+        projectId: "gasksa-de4ab",
+        storageBucket: "gasksa-de4ab.appspot.com",
+        messagingSenderId: "193816669511"
+    };
+    firebase.initializeApp(config);
+    var price
+
+    var ref=firebase.database().ref();
+    ref.child('pipeprice').set(price)
+logout=function () {
+    firebase.fireAuth.signOut();
+
+}
+})
